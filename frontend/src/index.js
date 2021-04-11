013562564fef12378b341c13bf3e17b0df630433
ysonbrian <znlsakxls@gmail.com>
2021-04-11 23:29:29 +0900

    CartScreen: Build Cart API

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,14 @@
-import Error404Screen from './screens/Error404Screen.js';
-import HomeScreen from './screens/HomeScreen.js';
-import ProductScreen from './screens/ProductScreen.js';
-import { parseRequestUrl } from './utils.js';
+import CartScreen from './screens/CartScreen';
+import Error404Screen from './screens/Error404Screen';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import { parseRequestUrl } from './utils';
 
 const routes = {
   '/': HomeScreen,
   '/product/:id': ProductScreen,
+  '/cart/:id': CartScreen,
+  '/cart': CartScreen,
 };
 
 const router = async () => {
---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,38 @@
+import { getProduct } from '../api';
+import { getCartItems, setCartItems } from '../localStorage';
+import { parseRequestUrl } from '../utils';
+
+const addToCart = (item, forceUpdate = false) => {
+  let cartItems = getCartItems();
+  const existItem = cartItems.find((x) => x.product === item.product);
+  if (existItem) {
+    cartItems = cartItems.map((x) =>
+      x.product === existItem.product ? item : x
+    );
+  } else {
+    cartItems = [...cartItems, item];
+  }
+  setCartItems(cartItems);
+};
+const CartScreen = {
+  after_render: () => {},
+  render: async () => {
+    const request = parseRequestUrl();
+    if (request.id) {
+      const product = await getProduct(request.id);
+      addToCart({
+        product: product._id,
+        name: product.name,
+        image: product.image,
+        price: product.price,
+        countInStock: product.countInStock,
+        qty: 1,
+      });
+    }
+    return `<div>Cart Screen</div>
+      <div>${getCartItems().length}</div>
+    `;
+  },
+};
+
+export default CartScreen;
